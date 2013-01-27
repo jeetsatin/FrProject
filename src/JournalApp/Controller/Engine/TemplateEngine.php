f23aabe636609ac23edb330108dbd03ed4487af2
unknown <Satinder@.(none)>
2013-01-27 22:05:33 +0530

    initial hierarchy

---
+++ src/JournalApp/Controller/Engine/TemplateEngine.php
@@ -0,0 +1,17 @@
+<?php
+
+Use Rain\Tpl;
+
+/**
+ * Description of TemplateEngine
+ *
+ * @author satin
+ */
+class TemplateEngine extends RainTPL{
+    
+    public function configOptions($options)
+    {
+        self::configure($options);
+    }
+    
+}
